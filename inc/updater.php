20d65896352e1ef06e6a123aa044061766f8e846
Carlo Stringaro <carlo.stringaro@gmail.com>
2019-07-30 20:36:26 +0200

    Add updater.inc in inc folder

---
+++ inc/updater.php
@@ -0,0 +1,18 @@
+<?php
+if (is_admin()) { //nota l'uso si is_admin(), serve per rassicurarci che ci troviamo nel backend
+  $config = array(
+    'slug' => plugin_basename(__FILE__), // l'abbreviazione del plugin
+    'proper_folder_name' => 'plugin-name', // il nome della cartella che contiene il nostro plugin
+    'api_url' => 'http://api.github.com/repos/nomeutente/nome-repository', // la GitHub API url del repository contenente il plugin
+    'raw_url' => 'http://raw.github.com/nomeutente/nome-repository/master', // la GitHub raw url del repository contenente il plugin
+    'github_url' => 'http://github.com/nomeutente/nome-repository', // la GitHub url del repository contenente il plugin
+    'zip_url' => 'http://github.com/nomeutente/nome-repository/zipball/master', // dove si trova l'archivio .zip del repository
+    'sslverify' => true, // se WordPress deve utilizzare un certificato SSL quando effettua il controllo sull'aggiornamento
+    'requires' => '3.0', // specificare quale versione di WordPress e' richiesta da questo plugin
+    'tested' => '3.3', // fino a che versione di WordPress hai testato il plugin?
+    'readme' => 'README.md', // quale file deve essere usato per controllare la versione del plugin?
+    'access_token' => '', // serve soltanto quando utilizziamo repository WordPress privati
+  );
+  new WP_GitHub_Updater($config);
+}
+?>
